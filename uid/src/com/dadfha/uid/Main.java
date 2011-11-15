161ec175af9cd418cc8d425b3d38aa940e27490b
nabito <nabito@gmail.com>
2011-11-16 03:18:49 +0900

    Add comment to test commit in the master branch

--- uid/src/com/dadfha/uid/Main.java
+++ uid/src/com/dadfha/uid/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		
 		// don't forget to exclude assert in production release: http://download.oracle.com/javase/1.4.2/docs/guide/lang/assert.html#enable-disable
 		UcodeRP ucrp = UcodeRP.getUcodeRP();
-		
+		// TODO remove this soon!
 		ucrp.hello();
 
 	}
